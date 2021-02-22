103ecd09215feb8c7597fa24b70e6ceffda37e6d
afernandezro7 <a.fernandez.ro7@gmail.com>
2021-02-21 22:42:26 -0500

    adding dinamic language to About Page

--- pages/[language]/quienes-somos.js
+++ pages/[language]/quienes-somos.js
@@ -1,90 +1,63 @@
 import Container from '../../components/layouts/Container'
 import Head from 'next/head'
+import { useSelector } from 'react-redux'
 
 
 export default function QuienesSomos() {
+
+    const {content:{ aboutPage }} = useSelector( state => state.lang );
+
     return (
         <Container >
             <Head>
-                <title>Quienes Somos</title>
+                <title>{ aboutPage.header }</title>
             </Head>
 
             <main className="quienesSomos-page">
                 < section className = "container cober--Items" > 
                     <div className="cober--header mb-3">
-                        <h1 className="cober--Title text-primary">CUBAASISTUR.COM</h1>
+                        <h1 className="cober--Title text-primary">{ aboutPage.h1 }</h1>
                     </div>
-                    <div className="cober--Item ">
-                        <div className="cober--ItemTitle text-primary d-flex justify-content-start ">
-                            <h2 className="cober--Tema text-primary ">QUIENES SOMOS</h2>
-                        </div>
-                        <div className="cober--ItemContent">
-                            <p>
-                                <b>CubaAsistur.com</b>
-                                es propiedad de Belraysa Tours &amp; Travel Group SA., Agencia de Viajes panameña
-                                con oficinas en Ciudad Panamá, La Habana y Nicaragua. La Agencia es la propietaria de
-                                <b>CubaAsistur.com</b>
-                                y de otros sitios especializados en turismo convencional, de salud, de
-                                congresos y eventos.
-                                <b>Belraysa Tours &amp; Travel Group SA</b>
-                                está radicada en Panamá con domicilio
-                                en Calle Eric del Valle e/ Vía Argentina y Alberto Navarro, Edif. Rapiventa, Planta Baja, El
-                                Cangrejo, Bella Vista Panamá.
-                                <b>Belraysa Tours &amp; Travel Group SA</b>
-                                a través de CubaAsistur opera
-                                en exclusivo el canal de ventas online de pólizas de seguro de
-                                <b>ASISTUR</b>, que es una sociedad
-                                mercantil cubana del sector de los seguros y los servicios financieros, con oficina central en La
-                                Habana, Prado No. 208 entre Trocadero y Colón, Habana Vieja, La Habana, teléfonos +53 7866 4499 /+53
-                                7866 8087.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="cober--Item ">
-                        <div className="cober--ItemTitle text-primary d-flex text-left ">
-                            <h2 className="cober--Tema text-primary ">CÓMO FUNCIONAN NUESTRAS PÓLIZAS</h2>
-                        </div>
-                        <div className="cober--ItemContent">
-                            <p>Las pólizas de CubaAsistur se activan realizando una llamada a la CENTRAL DE ALARMA a los teléfonos
-                            (53 7) 866 8527, 8339 y 8920 y comunicando su emergencia y número de póliza. Si su emergencia es
-                            médica puede acercarse directamente al centro de atención más cercano y mostrar la información
-                            relacionada con su póliza para que la misma sea activada o sea remitido al centro de atención donde
-                            le ofrecerán asistencia médica.</p>
-                        </div>
-                    </div>
-                    <div className="cober--Item">
-                        <div className="cober--ItemTitle text-primary d-flex text-left ">
-                            <h2 className="cober--Tema text-primary ">VENTAJAS DE CUBAASISTUR</h2>
-                        </div>
-                        <div className="cober--ItemContent text-left">
-                            <ul className="pl-0">
-                                <li>ASISTUR es la Compañía líder de seguros en Cuba
-                                    <ul>
-                                        <li>Experiencia desde 1991 en la prestación de servicios especializados de asistencia a los
-                                        viajeros.</li>
-                                        <li>Personal calificado</li>
-                                        <li>Una extensa red de proveedores</li>
-                                        <li>Central de Alarma disponible las 24 horas del día durante todo el año.</li>
-                                    </ul>
-                                </li>
-                                <li>Reserva online segura
-                                    <ul>
-                                        <li>Todos los datos que Usted proporcione en el proceso de reserva y pago con tarjeta de
-                                        crédito están protegidos a través de certificados digitales de nuestra pasarela de pago
-                                        Páguelo Fácil, que cuenta con poderosos procesos para la encriptación de información
-                                        sensible, garantizando que no puedan ser utilizados por terceros.</li>
-                                    </ul>
-                                </li>
-                                <li>Tecnología de avanzada.
-                                    <ul>
-                                        <li>Somos conscientes del valor del tiempo en la navegación por internet y en la toma de
-                                        decisiones de los usuarios. Es por ello que diseñamos un sitio web intuitivo y rápido,
-                                        que se adapta a cualquier dispositivo.</li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+
+                    {
+                        aboutPage.content.map( (item, index )=>(
+                            <div key={index} className="cober--Item">
+                                <div className="cober--ItemTitle text-primary d-flex justify-content-start ">
+                                    <h2 className="cober--Tema text-primary">{ item.title }</h2>
+                                </div>
+                                {
+                                    item.isList 
+                                        ? <div className="cober--ItemContent text-left">
+                                            <ul className="pl-0">
+                                                {
+                                                    item.items.map( (listItem,index)=>(
+                                                        <li key={index}>{listItem.header}
+                                                            <ul>
+                                                                {
+                                                                    listItem.content.map((sublistItem,index)=>(
+                                                                        <li key={index}>{ sublistItem }</li>
+                                                                    ))
+                                                                }
+                                                            </ul>
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                        : <div className="cober--ItemContent">
+                                            <p>
+                                                { item.p }
+                                            </p>
+                                        </div>
+                                }
+                                
+                            </div>
+                            
+
+                        ))
+                    }
+
+                    
                 </section>
 
             </main>
--- redux/store/tempData.js
+++ redux/store/tempData.js
@@ -68,9 +68,43 @@ export const tempContent={
             }
         ],
         copyRight: 'Copyright 2012 - 2020 |BELRAYSA S.A. | All Right Reserved | Belraysa Tours and Travel',
+    },
 
-        //
+    //Pages
+    aboutPage:{
+        header: 'Quienes Somos',
+        h1: 'CUBAASISTUR.COM',
+        content: [
+            {
+                isList: false,
+                title: 'QUIENES SOMOS',
+                p: 'CubaAsistur.com es propiedad de Belraysa Tours & Travel Group SA., Agencia de Viajes panameña con oficinas en Ciudad Panamá, La Habana y Nicaragua. La Agencia es la propietaria de CubaAsistur.com y de otros sitios especializados en turismo convencional, de salud, de congresos y eventos. Belraysa Tours & Travel Group SA está radicada en Panamá con domicilio en Calle Eric del Valle e/ Vía Argentina y Alberto Navarro, Edif. Rapiventa, Planta Baja, El Cangrejo, Bella Vista Panamá.Belraysa Tours & Travel Group SA a través de CubaAsistur opera en exclusivo el canal de ventas online de pólizas de seguro de ASISTUR, que es una sociedad mercantil cubana del sector de los seguros y los servicios financieros, con oficina central en La Habana, Prado No. 208 entre Trocadero y Colón, Habana Vieja, La Habana, teléfonos +53 7866 4499 /+53 7866 8087.',
+            },
+            {
+                isList: false,
+                title: 'CÓMO FUNCIONAN NUESTRAS PÓLIZAS',
+                p: 'Las pólizas de CubaAsistur se activan realizando una llamada a la CENTRAL DE ALARMA a los teléfonos (53 7) 866 8527, 8339 y 8920 y comunicando su emergencia y número de póliza. Si su emergencia es médica puede acercarse directamente al centro de atención más cercano y mostrar la información relacionada con su póliza para que la misma sea activada o sea remitido al centro de atención donde le ofrecerán asistencia médica.',
+            },
+            {
+                isList: true,
+                title: 'VENTAJAS DE CUBAASISTUR',
+                items:[
+                    {
+                        header:'ASISTUR es la Compañía líder de seguros en Cuba',
+                        content:['Experiencia desde 1991 en la prestación de servicios especializados de asistencia a los viajeros.', 'Personal calificado', 'Una extensa red de proveedores', 'Central de Alarma disponible las 24 horas del día durante todo el año.']
+                    },
+                    {
+                        header:'Reserva online segura',
+                        content:['Todos los datos que Usted proporcione en el proceso de reserva y pago con tarjeta de crédito están protegidos a través de certificados digitales de nuestra pasarela de pago Páguelo Fácil, que cuenta con poderosos procesos para la encriptación de información sensible, garantizando que no puedan ser utilizados por terceros.']
+                    },
+                    {
+                        header:'Tecnología de avanzada.',
+                        content:['Somos conscientes del valor del tiempo en la navegación por internet y en la toma de decisiones de los usuarios. Es por ello que diseñamos un sitio web intuitivo y rápido, que se adapta a cualquier dispositivo.']
+                    },
+                ]
 
+            }
+        ]
+    },
 
-    }
 }
